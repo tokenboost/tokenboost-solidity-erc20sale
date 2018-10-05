3d359ba0d4db6f14264e1c3f2a6961440d5daf06
Yoonjae Yoo <yoonjae@dnext.co>
2018-10-05 17:53:07 +0900

    Update ERC20Sale: 1. Determine if a sale ‘purchasable()’ if one of activatedStrategies return true. 2. Get token rate from the first activated strategy that returns tokenRate > 0. 3. Make ‘purchasable()’ and ‘getTokenAmount()’ public. Update ERC20SaleTemplate: InterfaceId_ERC20Sale.

--- contracts/sale/erc20/ERC20Sale.sol
+++ contracts/sale/erc20/ERC20Sale.sol
@@ -14,15 +14,15 @@ contract ERC20Sale is Sale {
         ^ this.template.selector ^ this.activate.selector
         ^ this.projectName.selector ^ this.projectSummary.selector ^ this.projectDescription.selector
         ^ this.logoUrl.selector ^ this.coverImageUrl.selector ^ this.websiteUrl.selector ^ this.whitepaperUrl.selector
-        ^ this.name.selector ^ this.weiRaised.selector ^ this.withdrawn.selector ^ this.ready.selector
+        ^ this.name.selector ^ this.weiRaised.selector ^ this.withdrawn.selector
         ^ this.started.selector ^ this.successful.selector ^ this.finished.selector ^ this.paymentOf.selector
         ^ bytes4(keccak256("update(string,string,string,string,string,string,string,string)"))
         ^ bytes4(keccak256("update(string,string,string,string,string,string,string,string,address)"))
         ^ this.addStrategy.selector ^ this.numberOfStrategies.selector ^ this.strategyAt.selector
-        ^ this.numberOfActivatedStrategies.selector ^ this.activatedStrategyAt.selector
-        ^ this.withdraw.selector ^ this.claimRefund.selector
+        ^ this.numberOfActivatedStrategies.selector ^ this.activatedStrategyAt.selector ^ this.withdraw.selector
+        ^ this.claimRefund.selector ^ this.purchasable.selector ^ this.getTokenAmount.selector
      */
-    bytes4 public constant InterfaceId_ERC20Sale = 0x95b5cfcb;
+    bytes4 public constant InterfaceId_ERC20Sale = 0x5f57e789;
 
     event TokenPurchase(
         address indexed purchaser,
@@ -70,36 +70,35 @@ contract ERC20Sale is Sale {
     function() whenActivated external payable {
         require(!finished());
         require(started());
-        require(_purchasable(msg.value));
+        require(purchasable(msg.value));
 
         increasePaymentOf(msg.sender, msg.value);
 
-        uint256 tokenAmount = _getTokenAmount(msg.value);
+        uint256 tokenAmount = getTokenAmount(msg.value);
         address tokenReceiver = _tokenReceiver(msg.sender, tokenAmount);
         require(_supplyTokens(tokenReceiver, tokenAmount));
 
         emit TokenPurchase(msg.sender, msg.value, tokenAmount);
     }
 
-    function _purchasable(uint256 _weiAmount) private view returns (bool) {
-        bool p = true;
+    function purchasable(uint256 _weiAmount) public view returns (bool) {
+        bool p = false;
         for (uint i = 0; i < activatedStrategies.length; i++) {
             ERC20SaleStrategy strategy = ERC20SaleStrategy(activatedStrategies[i]);
-            p = p && strategy.purchasable(msg.sender, _weiAmount);
+            p = p || strategy.purchasable(msg.sender, _weiAmount);
         }
         return p;
     }
 
-    function _getTokenAmount(uint256 _weiAmount) private view returns (uint256) {
-        uint256 tokenAmount = _weiAmount;
+    function getTokenAmount(uint256 _weiAmount) public view returns (uint256) {
         for (uint i = 0; i < activatedStrategies.length; i++) {
             ERC20SaleStrategy strategy = ERC20SaleStrategy(activatedStrategies[i]);
             uint256 rate = strategy.tokenRate(msg.sender, _weiAmount);
-            require(rate > 0);
-
-            tokenAmount = tokenAmount.mul(rate);
+            if (rate > 0) {
+                return _weiAmount.mul(rate);
+            }
         }
-        return tokenAmount;
+        return 0;
     }
 
     function _tokenReceiver(address _purchaser, uint256 _tokenAmount) private returns (address) {
