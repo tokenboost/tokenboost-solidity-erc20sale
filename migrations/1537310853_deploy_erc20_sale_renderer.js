a0885919314464e11c8bd20608d8b4dc2879d4ac
Yoonjae Yoo <yoonjae@dnext.co>
2018-10-07 21:40:58 +0900

    Overwrite ERC20SaleRenderer when migrating.

--- migrations/1537310853_deploy_erc20_sale_renderer.js
+++ migrations/1537310853_deploy_erc20_sale_renderer.js
@@ -8,7 +8,7 @@ const ERC20SaleRenderer = artifacts.require("ERC20SaleRenderer");
 
 module.exports = function(deployer) {
     return deployer.then(async () => {
-        let erc20SaleRenderer = await deployer.deploy(ERC20SaleRenderer);
+        let erc20SaleRenderer = await deployer.deploy(ERC20SaleRenderer, {overwrite: false});
         await Promise.all([
             await erc20SaleRenderer.setAdminWidgetRenderers([
                 FundsRaisedWidgetRenderer.address,
