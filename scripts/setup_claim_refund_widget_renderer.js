fa723bfbd15fcb562b8d03f729f4bd23a22bdd02
Yoonjae Yoo <yoonjae@dnext.co>
2018-10-07 21:45:01 +0900

    Update setup scripts.

--- scripts/setup_claim_refund_widget_renderer.js
+++ scripts/setup_claim_refund_widget_renderer.js
@@ -8,10 +8,10 @@ module.exports = async (callback) => {
             await renderer.setResource("en", "short_desc", "You can claim refund of your investment."),
             await renderer.setResource("en", "long_desc", "You can claim refund because the sale has failed. Fund will be returned to your address."),
             await renderer.setResource("en", "claim_refund_confirm", "Do you want to claim refund?"),
-            await renderer.setResource("en", "claim_refund", "환불 요청"),
-            await renderer.setResource("en", "short_desc", "투자금에 대해 환불을 요청할 수 있습니다."),
-            await renderer.setResource("en", "long_desc", "세일이 실패했기 때문에 환불 요청을 할 수 있습니다. 자금이 내 계좌로 전송됩니다."),
-            await renderer.setResource("en", "claim_refund_confirm", "환불 요청하시겠습니까?"),
+            await renderer.setResource("ko", "claim_refund", "환불 요청"),
+            await renderer.setResource("ko", "short_desc", "투자금에 대해 환불을 요청할 수 있습니다."),
+            await renderer.setResource("ko", "long_desc", "세일이 실패했기 때문에 환불 요청을 할 수 있습니다. 자금이 내 계좌로 전송됩니다."),
+            await renderer.setResource("ko", "claim_refund_confirm", "환불 요청하시겠습니까?"),
         ]);
 
         callback();
--- scripts/setup_funds_raised_widget_renderer.js
+++ scripts/setup_funds_raised_widget_renderer.js
@@ -7,7 +7,7 @@ module.exports = async (callback) => {
             await renderer.setResource("en", "funds_raised", "Funds Raised"),
             await renderer.setResource("en", "short_desc", "Total amount of ETH raised so far."),
             await renderer.setResource("en", "long_desc", "Total amount of ETH raised so far."),
-            await renderer.setResource("ko", "my_balance", "모금 금액"),
+            await renderer.setResource("ko", "funds_raised", "모금 금액"),
             await renderer.setResource("ko", "short_desc", "지금까지 모금된 총 ETH 금액."),
             await renderer.setResource("ko", "long_desc", "지금까지 모금된 총 ETH 금액."),
         ]);
