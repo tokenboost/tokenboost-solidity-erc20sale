c1b6d41139a70b7af8754e49d4992404424832de
Yoonjae Yoo <yoonjae@dnext.co>
2018-10-08 14:26:32 +0900

    Update a setup script.

--- scripts/setup_erc20_sale_inputs_renderer.js
+++ scripts/setup_erc20_sale_inputs_renderer.js
@@ -12,9 +12,10 @@ module.exports = async (callback) => {
             await renderer.setResource("en", "sale_name", "Round Name (ex: Private Sale, Presale, Public Sale)"),
             await renderer.setResource("en", "logo_url", "Logo URL (256x256)"),
             await renderer.setResource("en", "cover_image_url", "Cover Image URL"),
-            await renderer.setResource("en", "webiste_url", "Website URL"),
+            await renderer.setResource("en", "website_url", "Website URL"),
+            await renderer.setResource("en", "whitepaper_url", "Whitepaper URL"),
             await renderer.setResource("en", "update", "Update"),
-            await renderer.setResource("en", "update", "Do you want to update?"),
+            await renderer.setResource("en", "update_confirm", "Do you want to update?"),
             await renderer.setResource("ko", "token_address", "토큰 주소"),
             await renderer.setResource("ko", "project_name", "프로젝트 이름 (예: Ethereum - Blockchain App Platform)"),
             await renderer.setResource("ko", "project_summary", "프로젝트 요약"),
@@ -22,9 +23,10 @@ module.exports = async (callback) => {
             await renderer.setResource("ko", "sale_name", "라운드 이름 (ex: Private Sale, Presale, Public Sale)"),
             await renderer.setResource("ko", "logo_url", "로고 URL (256x256)"),
             await renderer.setResource("ko", "cover_image_url", "커버 이미지 URL"),
-            await renderer.setResource("ko", "webiste_url", "웹사이트 URL"),
+            await renderer.setResource("ko", "website_url", "웹사이트 URL"),
+            await renderer.setResource("ko", "whitepaper_url", "백서 URL"),
             await renderer.setResource("ko", "update", "업데이트"),
-            await renderer.setResource("ko", "update", "업데이트 하시겠습니까?"),
+            await renderer.setResource("ko", "update_confirm", "업데이트 하시겠습니까?"),
         ]);
 
         callback();
